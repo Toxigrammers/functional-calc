156840df1389c8d2e7025242914ad0a5418a31b0
Sansshrek <franci.pagliaccia13@gmail.com>
2021-05-03 16:26:33 +0200

    Update equationresolver.py

--- equationcontrols/equationresolver.py
+++ equationcontrols/equationresolver.py
@@ -5,23 +5,23 @@ import python.functionalcalc.equationcontrols.separateValues
 def firstDegree(equ):
     a,b = separateFirstDegree(equ)
     x = (-b)/a
-    print("Il risultato di x è "+x)
+    n = [x]
+    return n
 
 def secondDegree(equ):
     a,b,c = separateSecondDegree(equ)
-    if(checkDelta(a,b,c)):
-        n = Equation(a,b,c)
-        if(len(n) == 1):
-            print('Il risultato di x1 e x2 è '+str(n[0]))
-        else:
-            print('Il risultato di x1 è {} e x2 è {}'.format(n[0],n[1]))
+    if checkDelta(a,b,c):
+        x1,x2 = Equation(a,b,c)
+        n = [x1,x2]
+        return n
     else:
-        print("Calcolo impossibile")
+        print("\nCalcolo impossibile")
+        return False
 
 def checkDegree(equ):  # check equation degree
-    if(equ.contains('x^2')):
+    if 'x^2' in equ:
         n = 2
-    elif(equ.contains('x')):
+    elif 'x' in equ:
         n = 1
     else:
         n = 0
@@ -30,7 +30,7 @@ def checkDegree(equ):  # check equation degree
 def checkDelta(a,b,c):  # check if delta is less than 0
     check = False
     delta = (b*b)-(4*a*c)
-    if(delta>=0):
+    if delta>=0:
         check = True
     return check
 
@@ -39,20 +39,17 @@ def Equation(a,b,c):
     delta = math.sqrt(delta)
     n1 = (-b + delta)/(2*a)
     n2 = (-b - delta)/(2*a)
-    n = []
-    if(n1 == n2):
-        n.append(n1)
-    else:
-        n.append(n1)
-        n.append(n2)
-    return n
+    return n1,n2
+
+def solveEquation(equ):
+    if checkDegree(equ)==1:
+        return firstDegree(equ)
+    elif checkDegree(equ)==2:
+        return secondDegree(equ)
 
 scelta = "1"
 while(scelta != "si"):
     print("\nNo space and comma to separate values\nequation format example: -x^2,+3x,-2")
     equ = input("Inserire equazione : ")
-    if(checkDegree(equ)==1):
-        firstDegree(equ)
-    elif(checkDegree(equ)==2):
-        secondDegree(equ)
+    solveEquation(equ)
     scelta = input("Vuoi uscire? (si/no): ")
