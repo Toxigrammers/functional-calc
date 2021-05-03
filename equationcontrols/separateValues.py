16b54b4b0a38fc2247b3dc644282bd4d7efe73df
Sansshrek <franci.pagliaccia13@gmail.com>
2021-05-03 16:26:37 +0200

    Update separateValues.py

--- equationcontrols/separateValues.py
+++ equationcontrols/separateValues.py
@@ -9,26 +9,24 @@
 
 def separateFirstDegree(equ):
     split_equ = equ.split(',')
-    for i in range(len(split_equ)):
-        print(split_equ[i], end="\n")
     a = b = 0
-    if(len(split_equ) == 2):
+    if len(split_equ) == 2:
         n1 = split_equ[0]
         n2 = split_equ[1]
         pos = n1.find('x')
-        if(pos == 0):  # +1 is implied
+        if pos == 0 or n1[pos-1] == '+':  # +1 is implied
             a = 1
-        elif(n1[pos-1] == '-'):  # -1 is implied
+        elif n1[pos-1] == '-':  # -1 is implied
             a = -1
         else:
             a = int(n1[:pos])
         b = int(n2)
-    elif(len(split_equ) == 1):
+    elif len(split_equ) == 1:
         n1 = split_equ[0]
         pos = n1.find('x')
-        if(pos == 0):  # +1 is implied
+        if pos == 0 or n1[pos-1] == '+':  # +1 is implied
             a = 1
-        elif(n1[pos-1] == '-'):  # -1 is implied
+        elif n1[pos-1] == '-':  # -1 is implied
             a = -1
         else:
             a = int(n1[:pos])
@@ -36,58 +34,56 @@ def separateFirstDegree(equ):
 
 def separateSecondDegree(equ):
     split_equ = equ.split(',')
-    for i in range(len(split_equ)):
-        print(split_equ[i], end="\n")
     a = b = c = 0
-    if(len(split_equ) == 3):
+    if len(split_equ) == 3:
         n1 = split_equ[0]  # x^2  a
         n2 = split_equ[1]  # x    b
         n3 = split_equ[2]  #      c
         pos = n1.find('x^2')
-        if(pos == 0):  # +1 is implied
+        if pos == 0 or n1[pos-1] == '+':  # +1 is implied
             a = 1
-        elif(n1[pos-1] == '-'): # -1 is implied
+        elif n1[pos-1] == '-': # -1 is implied
             a = -1
         else:
             a = int(n1[:pos])
         pos = n2.find('x')
-        if(pos == 0):
+        if n2[pos-1] == '+':
             b = 1
-        elif(n2[pos-1] == '-'):
+        elif n2[pos-1] == '-':
             b = -1
         else:
             b = int(n2[:pos])
         c = int(n3)
-    elif(len(split_equ) == 2):
+    elif len(split_equ) == 2:
         n1 = split_equ[0]  # x^2  a
         pos = n1.find('x^2')
-        if(pos == 0):  # +1 is implied
+        if pos == 0 or n1[pos-1] == '+':  # +1 is implied
             a = 1
-        elif(n1[pos-1] == '-'): # -1 is implied
+        elif n1[pos-1] == '-': # -1 is implied
             a = -1
         else:
             a = int(n1[:pos])
-        if(split_equ[1].contains('x')):
+        if 'x' in split_equ[1]:
             n2 = split_equ[1]  # x  b
             pos = n2.find('x')
-            if(pos == 0):
+            if n2[pos-1] == '+':
                 b = 1
-            elif(n2[pos-1] == '-'):
+            elif n2[pos-1] == '-':
                 b = -1
             else:
                 b = int(n2[:pos])
         else:
             c = int(split_equ[1])
-    elif(len(split_equ) == 1):
+    elif len(split_equ) == 1:
         a = int(split_equ[0])
     return a,b,c
 
 str = input("eqauzione 1° o 2° grado? ")
 n = input("Inserire equazione: ")
-if(str == "2"):
+if str == "2":
     a,b,c = separateSecondDegree(n)
     print("a = {}, b = {}, c = {}".format(a,b,c))
-elif(str == "1"):
+elif str == "1":
     a,b = separateFirstDegree(n)
     print("a = {}, b = {}".format(a,b))
 else:
