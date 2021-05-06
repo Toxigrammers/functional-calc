c9120477f8fdbacc7ae04964fa6a3c4cb413dd19
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-06 11:11:23 +0200

    modifying gui.
    updated .gitignore

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,7 @@
-from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivymd.uix.button import MDRectangleFlatButton
 
 
 KV = '''
@@ -11,11 +12,20 @@ BoxLayout:
         id: numeratore
         hint_text: "inserisci il numeratore"
         helper_text: "There will always be a mistake"
-        pos_hint: {"center_x": .5, "center_y": .5}
+        pos_hint: {"center_x": .5, "center_y": .8}
 '''
 
 class MainApp(MDApp):
     def build(self):
-        return Builder.load_string(KV)
+        screen = Screen()
+        screen.add_widget(
+            MDRectangleFlatButton(
+                text="Vaffanculo",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+        ),
+        username = Builder.load_string(KV)
+        screen.add_widget(username)
+        return screen
 
 MainApp().run()
