84c682b77c9e5f2e4270de7c886fb31116c0cf3b
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-06 18:35:48 +0200

    changing ui.

--- gui/gui.py
+++ gui/gui.py
@@ -1,45 +1,30 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
+from styled_components import *
+from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRectangleFlatButton
-from kivy.uix.behaviors import ButtonBehavior
 
-box_numeratore = '''
-BoxLayout:
-    padding: "10dp"
-
-    MDTextField:
-        id: numeratore
-        hint_text: "inserisci il numeratore"
-        helper_text: "There will always be a mistake"
-        pos_hint: {"center_x": .5, "center_y": .8}
-'''
-
-box_denominatore = '''
-BoxLayout:
-    padding: "10dp"
-    MDTextField:
-        id: numeratore
-        hint_text: "inserisci il denominatore"
-        helper_text: "There will always be a mistake"
-        pos_hint: {"center_x": .5, "center_y": .7}
-'''
 
 class MainApp(MDApp):
     def build(self):
         screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(
+        self.theme_cls.primary_palette = "Green"
+        screen.add_widget (
+            MDRectangleFlatButton (
                 text="Press me!",
                 pos_hint={"center_x": 0.5, "center_y": 0.5},
-                on_press=self.event
-            )
-        ),
-        numeratore = Builder.load_string(box_numeratore)
-        denominatore = Builder.load_string(box_denominatore)
-        screen.add_widget(numeratore)
-        screen.add_widget(denominatore)
+                on_release=self.event )
+        )
+        self.numeratore = Builder.load_string(box_numeratore)
+        self.denominatore = Builder.load_string(box_denominatore)
+        screen.add_widget(self.numeratore)
+        screen.add_widget(self.denominatore)
         return screen
-    def event(self, obj):
-        print("added event!")
+
+    def event(self,obj):
+        # self.numeratore.text = str(self.numeratore)
+        # print(self.numeratore.text)
+        print("miao")
+
+
 MainApp().run()
---
+++ gui/styled_components.py
@@ -0,0 +1,15 @@
+box_numeratore = '''
+    MDTextField:
+        id: numeratore
+        hint_text: "inserisci il numeratore"
+        helper_text: "There will always be a mistake"
+        pos_hint: {"center_x": .5, "center_y": .8}
+'''
+
+box_denominatore = '''
+    MDTextField:
+        id: numeratore
+        hint_text: "inserisci il denominatore"
+        helper_text: "There will always be a mistake"
+        pos_hint: {"center_x": .5, "center_y": .7}
+'''
