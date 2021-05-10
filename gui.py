f01e98ece7471ab69c11860e212da7e007c1f988
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-10 11:40:37 +0200

    code refactoring, updated .gitignore.

--- gui.py
+++ gui.py
@@ -4,24 +4,24 @@ from kivymd.uix.dialog import MDDialog
 from gui_style.styled_components import *
 from equation_controls.equation_resolver import *
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from function_type.fractional_rational import rational_function
 
 class MainScreen(Screen):
     pass
 
 
-class LoginScreen(Screen):
+class EquazioniSecondo(Screen):
     pass
 
 
-class RegisterScreen(Screen):
+class RazionaliFratte(Screen):
     pass
 
 
 sm = ScreenManager()
 sm.add_widget(MainScreen(name='main'))
-sm.add_widget(LoginScreen(name='login'))
-sm.add_widget(RegisterScreen(name='register'))
+sm.add_widget(EquazioniSecondo(name='login'))
+sm.add_widget(RazionaliFratte(name='register'))
 
 
 class DemoApp(MDApp):
@@ -33,16 +33,19 @@ class DemoApp(MDApp):
         self.screen = Builder.load_string(KV)
         return self.screen
 
-    def show_alert_dialog(self):
+    def get_equation_result(self):
         equation = self.root.get_screen('login')
-        equ = str(equation.ids.email.text)
+        equ = str(equazione.ids.equazione.text)
         sol = solve_equation(equ)
         if not self.dialog:
             self.dialog = MDDialog(
-                text=sol,
+                text=str(sol),
             )
         self.dialog.open()
         
-
+    def get_function_result(self):
+        x1 = self.root.get_screen('numeratore')
+        x2 = self.root.get_screen('denominator')
+        rational_function(x1, x2)
 if __name__ == '__main__':
     DemoApp().run()
